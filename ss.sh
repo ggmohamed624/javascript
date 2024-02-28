2855f1dcca5518ed4ea3bb0be14dac5011893e0e
ggmohamed624 <110822386+ggmohamed624@users.noreply.github.com>
2024-02-28 21:14:25 +0200

    Update ss.sh

--- ss.sh
+++ ss.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-# Start ffmpeg stream in the background
-nohup ffmpeg -i http://193.58.152.27:7788/Al-Fajer-3/index.m3u8 -c:v copy  -c:a aac -b:a 128k -f flv rtmp://vsu.mycdn.me/input/6921293278142_5942804286142_s3sqf5qmby &
-
-# Sleep to keep the script running
-sleep infinity
+nohup ffmpeg -i http://193.58.152.27:7788/Yes-Drama/index.m3u8 -c:v copy -c:a aac -b:a 128k -f flv rtmp://vsu.mycdn.me/input/6921349180350_5942902262462_43ejpobvcu &
